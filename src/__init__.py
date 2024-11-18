bcd15026ef1caea2929eba75af727329dd25dfd9
RaulDeMaio <raul.demaio@openeconomics.com>
2024-11-18 09:33:46 +0100

    updated init py files

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-from ras_balancer import *
---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+"""
+Test suite for the RAS Balancer package.
+
+This package contains unit tests for:
+- Core RAS balancing functionality
+- Matrix generation utilities
+- Matrix shocking capabilities
+"""
+
+import os
+import sys
+
+# Add the src directory to the Python path for testing
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
